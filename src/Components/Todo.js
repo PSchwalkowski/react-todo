78219ca16e6a4ffa29ddf17d830893e65c5b0bff
Paweł Schwałkowski <p.schwalkowski@gmail.com>
2017-12-21 18:13:54 +0100

    create and complete todo

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -4,27 +4,38 @@ class Todo extends Component {
 
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			todos: [
 				this.createTodo('Make a cake', false),
-				this.createTodo('Go for beer', true),
+				this.createTodo('Go for beer', false),
 			]
 		}
+
+		this.handleCreateTodo = this.handleCreateTodo.bind(this);
+		this.handleCompleteTodo = this.handleCompleteTodo.bind(this);
 	}
 
+	/**
+	 * pseudo model
+	 *
+	 * @param title
+	 * @param completed
+	 * @returns {{title: *, completed: *}}
+	 */
 	createTodo(title, completed) {
 		return {
 			title: title,
-			completed: completed,
+			completed: completed || false,
 		}
 	}
 
 	listTodos() {
 		const list = this.state.todos.map((todo, index) =>
 			<tr key={index}>
-				<td>{todo.title}</td>
-				<td><input type="checkbox" name="completed" checked={todo.completed}/></td>
-				<td><input type="checkbox" name="remove"/></td>
+				<td>{todo.completed? 1 : 0} {todo.title}</td>
+				<td><button name="complete" value={index} onClick={this.handleCompleteTodo}>V</button></td>
+				<td><button name="remove" value={index}>X</button></td>
 			</tr>
 		);
 
@@ -40,10 +51,10 @@ class Todo extends Component {
 				<tbody>{list}</tbody>
 				<tfoot>
 					<tr>
-						<td><input type="text" name="title" placeholder="Buy new avr..." /></td>
+						<td><input type="text" name="title" placeholder="Buy new avr..." id="todo-title"/></td>
 						<td></td>
 						<td>
-							<button type="submit">Create</button>
+							<button type="submit" onClick={this.handleCreateTodo}>Create</button>
 						</td>
 					</tr>
 				</tfoot>
@@ -61,6 +72,45 @@ class Todo extends Component {
 				</div>
 			</div>
 		);
+	}
+
+	handleCreateTodo(event) {
+		const titleinput = document.getElementById('todo-title');
+
+		if (!titleinput.value.length) {
+			alert('Please provide todo title.');
+			return;
+		}
+
+		var todos = this.state.todos;
+		todos.push(
+			this.createTodo(titleinput.value)
+		);
+		this.setState({
+			todos: todos
+		});
+
+		titleinput.value = null;
+	}
+
+	handleCompleteTodo(event) {
+		const button = event.target;
+
+		var todos = this.state.todos.map((todo, index) => {
+			if (button.value == index)
+				todo.completed = !todo.completed;
+
+			return todo;
+		})
+
+		this.setState({
+			todos: todos
+		});
+
+		if (button.innerText === 'V')
+			button.innerText = 'X'
+		else
+			button.innerText = 'V';
 	}
 }
 
